da1b9d66e9e86c4271065096eda61a6f6013b681
Empty Keys <github@emptykeys.com>
2014-06-14 13:16:30 +0200

    Added more UI controls

--- BasicUI_MonoGame/GameUILibrary/BasicUI.xaml.cs
+++ BasicUI_MonoGame/GameUILibrary/BasicUI.xaml.cs
@@ -25,11 +25,25 @@ namespace EmptyKeys.UserInterface.Generated {
         
         private StackPanel e_4;
         
+        private ComboBox combo;
+        
         private Button button1;
         
         private Button button2;
         
         private Button button3;
+        
+        private Button button4;
+        
+        private Button button5;
+        
+        private Button button6;
+        
+        private TextBox textBox;
+        
+        private CheckBox checkBox;
+        
+        private TabControl e_5;
         
         public BasicUI(int width, int height, GraphicsDevice graphicsDevice, int nativeScreenWidth, int nativeScreenHeight) : 
                 base(width, height, graphicsDevice, nativeScreenWidth, nativeScreenHeight) {
@@ -50,11 +64,10 @@ namespace EmptyKeys.UserInterface.Generated {
             this.Content = this.e_2;
             this.e_2.Name = "e_2";
             RowDefinition row_e_2_0 = new RowDefinition();
+            row_e_2_0.Height = new GridLength(40F, GridUnitType.Pixel);
             this.e_2.RowDefinitions.Add(row_e_2_0);
             RowDefinition row_e_2_1 = new RowDefinition();
             this.e_2.RowDefinitions.Add(row_e_2_1);
-            RowDefinition row_e_2_2 = new RowDefinition();
-            this.e_2.RowDefinitions.Add(row_e_2_2);
             ColumnDefinition col_e_2_0 = new ColumnDefinition();
             this.e_2.ColumnDefinitions.Add(col_e_2_0);
             // e_3 element
@@ -63,19 +76,26 @@ namespace EmptyKeys.UserInterface.Generated {
             this.e_3.Name = "e_3";
             this.e_3.HorizontalAlignment = HorizontalAlignment.Center;
             this.e_3.VerticalAlignment = VerticalAlignment.Center;
-            this.e_3.Text = "Text";
+            this.e_3.Text = "Basic UI Example";
             // e_4 element
             this.e_4 = new StackPanel();
             this.e_2.Children.Add(this.e_4);
             this.e_4.Name = "e_4";
             this.e_4.Background = new SolidColorBrush(new Color(0, 0, 0, 255));
             Grid.SetRow(this.e_4, 1);
+            // combo element
+            this.combo = new ComboBox();
+            this.e_4.Children.Add(this.combo);
+            this.combo.Name = "combo";
+            this.combo.Width = 200F;
+            this.combo.Margin = new Thickness(5F, 5F, 5F, 5F);
+            this.combo.ItemsSource = this.Get_combo_Items();
             // button1 element
             this.button1 = new Button();
             this.e_4.Children.Add(this.button1);
             this.button1.Name = "button1";
             this.button1.Height = 30F;
-            this.button1.Width = 100F;
+            this.button1.Width = 200F;
             this.button1.Margin = new Thickness(5F, 5F, 5F, 5F);
             this.button1.Content = "1";
             // button2 element
@@ -83,7 +103,7 @@ namespace EmptyKeys.UserInterface.Generated {
             this.e_4.Children.Add(this.button2);
             this.button2.Name = "button2";
             this.button2.Height = 30F;
-            this.button2.Width = 100F;
+            this.button2.Width = 200F;
             this.button2.Margin = new Thickness(5F, 5F, 5F, 5F);
             this.button2.Content = "2";
             // button3 element
@@ -91,9 +111,53 @@ namespace EmptyKeys.UserInterface.Generated {
             this.e_4.Children.Add(this.button3);
             this.button3.Name = "button3";
             this.button3.Height = 30F;
-            this.button3.Width = 100F;
+            this.button3.Width = 200F;
             this.button3.Margin = new Thickness(5F, 5F, 5F, 5F);
             this.button3.Content = "3";
+            // button4 element
+            this.button4 = new Button();
+            this.e_4.Children.Add(this.button4);
+            this.button4.Name = "button4";
+            this.button4.Height = 30F;
+            this.button4.Width = 200F;
+            this.button4.Margin = new Thickness(5F, 5F, 5F, 5F);
+            this.button4.Content = "4";
+            // button5 element
+            this.button5 = new Button();
+            this.e_4.Children.Add(this.button5);
+            this.button5.Name = "button5";
+            this.button5.Height = 30F;
+            this.button5.Width = 200F;
+            this.button5.Margin = new Thickness(5F, 5F, 5F, 5F);
+            this.button5.Content = "5";
+            // button6 element
+            this.button6 = new Button();
+            this.e_4.Children.Add(this.button6);
+            this.button6.Name = "button6";
+            this.button6.Height = 30F;
+            this.button6.Width = 200F;
+            this.button6.Margin = new Thickness(5F, 5F, 5F, 5F);
+            this.button6.Content = "6";
+            // textBox element
+            this.textBox = new TextBox();
+            this.e_4.Children.Add(this.textBox);
+            this.textBox.Name = "textBox";
+            this.textBox.Width = 200F;
+            this.textBox.Margin = new Thickness(5F, 5F, 5F, 5F);
+            this.textBox.Text = "Text Box";
+            // checkBox element
+            this.checkBox = new CheckBox();
+            this.e_4.Children.Add(this.checkBox);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Margin = new Thickness(5F, 5F, 5F, 5F);
+            this.checkBox.HorizontalAlignment = HorizontalAlignment.Center;
+            this.checkBox.Content = "Check Box";
+            // e_5 element
+            this.e_5 = new TabControl();
+            this.e_4.Children.Add(this.e_5);
+            this.e_5.Name = "e_5";
+            this.e_5.Width = 400F;
+            this.e_5.ItemsSource = this.Get_e_5_Items();
         }
         
         private UIElement _s_S_0_ctMethod(UIElement parent) {
@@ -118,6 +182,49 @@ namespace EmptyKeys.UserInterface.Generated {
             binding_e_1_Content.Source = parent;
             e_1.SetBinding(ContentPresenter.ContentProperty, binding_e_1_Content);
             return e_0;
+        }
+        
+        private System.Collections.ObjectModel.ObservableCollection<object> Get_combo_Items() {
+            System.Collections.ObjectModel.ObservableCollection<object> items = new System.Collections.ObjectModel.ObservableCollection<object>();
+            // item1 element
+            ComboBoxItem item1 = new ComboBoxItem();
+            item1.Name = "item1";
+            item1.Content = "Item 1";
+            items.Add(item1);
+            // item2 element
+            ComboBoxItem item2 = new ComboBoxItem();
+            item2.Name = "item2";
+            item2.Content = "Item 2";
+            items.Add(item2);
+            // item3 element
+            ComboBoxItem item3 = new ComboBoxItem();
+            item3.Name = "item3";
+            item3.Content = "Item 3";
+            items.Add(item3);
+            return items;
+        }
+        
+        private System.Collections.ObjectModel.ObservableCollection<object> Get_e_5_Items() {
+            System.Collections.ObjectModel.ObservableCollection<object> items = new System.Collections.ObjectModel.ObservableCollection<object>();
+            // e_6 element
+            TabItem e_6 = new TabItem();
+            e_6.Name = "e_6";
+            e_6.Content = "Tab 1 Content";
+            e_6.Header = "Tab 1";
+            items.Add(e_6);
+            // e_7 element
+            TabItem e_7 = new TabItem();
+            e_7.Name = "e_7";
+            e_7.Content = "Tab 2 Content";
+            e_7.Header = "Tab 2";
+            items.Add(e_7);
+            // e_8 element
+            TabItem e_8 = new TabItem();
+            e_8.Name = "e_8";
+            e_8.Content = "Tab 3 Content";
+            e_8.Header = "Tab 3";
+            items.Add(e_8);
+            return items;
         }
     }
 }
