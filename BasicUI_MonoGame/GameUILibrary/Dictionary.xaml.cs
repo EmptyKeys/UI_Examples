084ba68f80f32a7ccd85176e0840948b3ce1c476
Empty Keys <github@emptykeys.com>
2014-06-24 17:48:16 +0200

    Added Resource Dictionary

---
+++ BasicUI_MonoGame/GameUILibrary/Dictionary.xaml.cs
@@ -0,0 +1,40 @@
+// -----------------------------------------------------------
+//  
+//  This file was generated, please do not modify.
+//  
+// -----------------------------------------------------------
+namespace EmptyKeys.UserInterface.Generated {
+    using System;
+    using System.CodeDom.Compiler;
+    using System.Collections.ObjectModel;
+    using EmptyKeys.UserInterface;
+    using EmptyKeys.UserInterface.Data;
+    using EmptyKeys.UserInterface.Controls;
+    using EmptyKeys.UserInterface.Controls.Primitives;
+    using EmptyKeys.UserInterface.Media;
+    using EmptyKeys.UserInterface.Themes;
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+    
+    
+    [GeneratedCodeAttribute("Empty Keys UI Generator", "0.9.0.0")]
+    public sealed class Dictionary : ResourceDictionary {
+        
+        private static Dictionary singleton = new Dictionary();
+        
+        public Dictionary() {
+            this.InitializeResources();
+        }
+        
+        public static Dictionary Instance {
+            get {
+                return singleton;
+            }
+        }
+        
+        private void InitializeResources() {
+            // Resource - [TitleResource] String
+            this.Add("TitleResource", "Basic UI Example");
+        }
+    }
+}
