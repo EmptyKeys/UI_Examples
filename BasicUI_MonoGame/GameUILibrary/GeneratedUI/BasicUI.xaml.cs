3c6eef86549a57d5babb42c79c3086287c5e8d4c
Empty Keys <github@emptykeys.com>
2014-08-11 12:19:15 +0200

    New version

--- BasicUI_MonoGame/GameUILibrary/GeneratedUI/BasicUI.xaml.cs
+++ BasicUI_MonoGame/GameUILibrary/GeneratedUI/BasicUI.xaml.cs
@@ -18,7 +18,7 @@ namespace EmptyKeys.UserInterface.Generated {
     using Microsoft.Xna.Framework.Graphics;
     
     
-    [GeneratedCodeAttribute("Empty Keys UI Generator", "0.9.4.0")]
+    [GeneratedCodeAttribute("Empty Keys UI Generator", "0.9.6.0")]
     public partial class BasicUI : UIRoot {
         
         private Grid e_0;
