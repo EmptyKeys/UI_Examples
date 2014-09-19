848a66fe872c5f90c68ff7a7b85042468a3e38a5
Empty Keys <github@emptykeys.com>
2014-09-19 16:26:22 +0200

    Message Box button localization example

--- BasicUI_SunBurn/BasicUI_Pro/GameLibrary/GeneratedUI/Dictionary.xaml.cs
+++ BasicUI_SunBurn/BasicUI_Pro/GameLibrary/GeneratedUI/Dictionary.xaml.cs
@@ -33,16 +33,8 @@ namespace EmptyKeys.UserInterface.Generated {
         }
         
         private void InitializeResources() {
-            // Resource - [buttonStyle] Style
-            var r_0_s_bo = this[typeof(Button)];
-            Style r_0_s = new Style(typeof(Button), r_0_s_bo as Style);
-            Setter r_0_s_S_0 = new Setter(Button.BackgroundProperty, new SolidColorBrush(new Color(255, 140, 0, 255)));
-            r_0_s.Setters.Add(r_0_s_S_0);
-            Setter r_0_s_S_1 = new Setter(Button.WidthProperty, 200F);
-            r_0_s.Setters.Add(r_0_s_S_1);
-            Setter r_0_s_S_2 = new Setter(Button.PaddingProperty, new Thickness(2F));
-            r_0_s.Setters.Add(r_0_s_S_2);
-            this.Add("buttonStyle", r_0_s);
+            // Resource - [ToolTipText] String
+            this.Add("ToolTipText", "Click to open message box");
             // Resource - [TitleResource] String
             this.Add("TitleResource", "Basic UI Example");
             // Resource - [logoSunburn] BitmapImage
@@ -50,24 +42,34 @@ namespace EmptyKeys.UserInterface.Generated {
             r_2_bm.TextureAsset = "Images/SunBurn";
             ImageManager.Instance.AddImage("Images/SunBurn");
             this.Add("logoSunburn", r_2_bm);
+            // Resource - [buttonStyle] Style
+            var r_3_s_bo = this[typeof(Button)];
+            Style r_3_s = new Style(typeof(Button), r_3_s_bo as Style);
+            Setter r_3_s_S_0 = new Setter(Button.BackgroundProperty, new SolidColorBrush(new Color(255, 140, 0, 255)));
+            r_3_s.Setters.Add(r_3_s_S_0);
+            Setter r_3_s_S_1 = new Setter(Button.WidthProperty, 200F);
+            r_3_s.Setters.Add(r_3_s_S_1);
+            Setter r_3_s_S_2 = new Setter(Button.PaddingProperty, new Thickness(2F));
+            r_3_s.Setters.Add(r_3_s_S_2);
+            this.Add("buttonStyle", r_3_s);
             // Resource - [logoEmptyKeys] BitmapImage
-            BitmapImage r_3_bm = new BitmapImage();
-            r_3_bm.TextureAsset = "Images/EmptyKeysLogoTextSmall";
+            BitmapImage r_4_bm = new BitmapImage();
+            r_4_bm.TextureAsset = "Images/EmptyKeysLogoTextSmall";
             ImageManager.Instance.AddImage("Images/EmptyKeysLogoTextSmall");
-            this.Add("logoEmptyKeys", r_3_bm);
+            this.Add("logoEmptyKeys", r_4_bm);
+            // Resource - [MessageBoxButtonYes] String
+            this.Add("MessageBoxButtonYes", "Yes!");
             // Resource - [Sounds] SoundSourceCollection
-            var r_4_sounds = new SoundSourceCollection();
-            r_4_sounds.Add(new SoundSource { SoundType = SoundType.ButtonsClick, SoundAsset = "Click" });
+            var r_6_sounds = new SoundSourceCollection();
+            r_6_sounds.Add(new SoundSource { SoundType = SoundType.ButtonsClick, SoundAsset = "Click" });
             SoundManager.Instance.AddSound("Click");
-            r_4_sounds.Add(new SoundSource { SoundType = SoundType.TextBoxKeyPress, SoundAsset = "KeyPress" });
+            r_6_sounds.Add(new SoundSource { SoundType = SoundType.TextBoxKeyPress, SoundAsset = "KeyPress" });
             SoundManager.Instance.AddSound("KeyPress");
-            r_4_sounds.Add(new SoundSource { SoundType = SoundType.TabControlMove, SoundAsset = "Move" });
+            r_6_sounds.Add(new SoundSource { SoundType = SoundType.TabControlMove, SoundAsset = "Move" });
             SoundManager.Instance.AddSound("Move");
-            r_4_sounds.Add(new SoundSource { SoundType = SoundType.TabControlSelect, SoundAsset = "Select" });
+            r_6_sounds.Add(new SoundSource { SoundType = SoundType.TabControlSelect, SoundAsset = "Select" });
             SoundManager.Instance.AddSound("Select");
-            this.Add("Sounds", r_4_sounds);
-            // Resource - [ToolTipText] String
-            this.Add("ToolTipText", "Click to open message box");
+            this.Add("Sounds", r_6_sounds);
         }
     }
 }
