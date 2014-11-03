09c0a5369a302dfc2c16e0a4f15d09df196e21aa
Empty Keys <github@emptykeys.com>
2014-11-03 12:47:11 +0100

    New Version of UI lib

--- BasicUI_MonoGame/GameUILibrary/GeneratedUI/Dictionary.xaml.cs
+++ BasicUI_MonoGame/GameUILibrary/GeneratedUI/Dictionary.xaml.cs
@@ -18,7 +18,7 @@ namespace EmptyKeys.UserInterface.Generated {
     using Microsoft.Xna.Framework.Graphics;
     
     
-    [GeneratedCodeAttribute("Empty Keys UI Generator", "1.0.1.0")]
+    [GeneratedCodeAttribute("Empty Keys UI Generator", "1.1.0.0")]
     public sealed class Dictionary : ResourceDictionary {
         
         private static Dictionary singleton = new Dictionary();
