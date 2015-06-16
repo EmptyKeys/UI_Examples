39fdf468d83edc71caf6fdd9311ad15ccc60ac49
Empty Keys <unknown-user@192.168.1.109>
2015-06-16 20:05:31 +0200

    OSX Update

--- BasicUI_MonoGame/BasicUI_MonoGame_OSX/Game1.cs
+++ BasicUI_MonoGame/BasicUI_MonoGame_OSX/Game1.cs
@@ -6,12 +6,14 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Storage;
-using Microsoft.Xna.Framework.GamerServices;
 using EmptyKeys.UserInterface.Generated;
 using EmptyKeys.UserInterface;
 using EmptyKeys.UserInterface.Input;
 using EmptyKeys.UserInterface.Media;
 using GameUILibrary;
+using EmptyKeys.UserInterface.Debug;
+
+
 #endregion
 
 namespace BasicUI_MonoGame_OSX
@@ -19,107 +21,118 @@ namespace BasicUI_MonoGame_OSX
     /// <summary>
     /// This is the main type for your game
     /// </summary>
-    public class Game1 : Game
-    {
-        GraphicsDeviceManager graphics;
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager graphics;        
 
-        private int nativeScreenWidth;
-        private int nativeScreenHeight;
+		private int nativeScreenWidth;
+		private int nativeScreenHeight;
 
-        private BasicUI basicUI;
+		private BasicUI basicUI;
+		private DebugViewModel debug;
 
-        public Game1()
-            : base()
-        {
-            graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            graphics.PreparingDeviceSettings += graphics_PreparingDeviceSettings;
-        }
+		public Game1()
+			: base()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			graphics.PreparingDeviceSettings += graphics_PreparingDeviceSettings;
+			graphics.DeviceCreated += graphics_DeviceCreated;
 
-        private void graphics_PreparingDeviceSettings(object sender, PreparingDeviceSettingsEventArgs e)
-        {
-            nativeScreenWidth = graphics.PreferredBackBufferWidth;
-            nativeScreenHeight = graphics.PreferredBackBufferHeight;
+		}
 
-            graphics.PreferredBackBufferWidth = 1280;
-            graphics.PreferredBackBufferHeight = 720;
-            graphics.PreferMultiSampling = true;
-            graphics.PreferredDepthStencilFormat = DepthFormat.Depth24Stencil8;
-        }
+		void graphics_DeviceCreated(object sender, EventArgs e)
+		{
+			Engine engine = new MonoGameEngine(GraphicsDevice, nativeScreenWidth, nativeScreenHeight);
+		}
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
+		private void graphics_PreparingDeviceSettings(object sender, PreparingDeviceSettingsEventArgs e)
+		{
+			nativeScreenWidth = graphics.PreferredBackBufferWidth;
+			nativeScreenHeight = graphics.PreferredBackBufferHeight;
 
-            base.Initialize();
-        }
+			graphics.PreferredBackBufferWidth = 1280;
+			graphics.PreferredBackBufferHeight = 720;
+			graphics.PreferMultiSampling = true;
+			graphics.PreferredDepthStencilFormat = DepthFormat.Depth24Stencil8;
+		}
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
-        protected override void LoadContent()
-        {
-            this.IsMouseVisible = true;
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
 
-            FontManager.DefaultFont = Content.Load<SpriteFont>("Segoe_UI_10_Regular");
-            Viewport viewport = GraphicsDevice.Viewport;
-            basicUI = new BasicUI(viewport.Width, viewport.Height, this.GraphicsDevice, nativeScreenWidth, nativeScreenHeight);
-            basicUI.DataContext = new BasicUIViewModel();
-            FontManager.Instance.LoadFonts(Content);
-            ImageManager.Instance.LoadImages(Content);
-            SoundManager.Instance.LoadSounds(Content);
+			base.Initialize();
+		}
 
-            RelayCommand command = new RelayCommand(new Action<object>(ExitEvent));
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			this.IsMouseVisible = true;
 
-            KeyBinding keyBinding = new KeyBinding(command, Keys.Escape, ModifierKeys.None);
-            basicUI.InputBindings.Add(keyBinding);
-        }
+			SpriteFont font = Content.Load<SpriteFont>("Segoe_UI_10_Regular");
+			FontManager.DefaultFont = Engine.Instance.Renderer.CreateFont(font);
+			Viewport viewport = GraphicsDevice.Viewport;
+			basicUI = new BasicUI(viewport.Width, viewport.Height);
+			basicUI.DataContext = new BasicUIViewModel();
+			debug = new DebugViewModel(basicUI);
+			FontManager.Instance.LoadFonts(Content);
+			ImageManager.Instance.LoadImages(Content);
+			SoundManager.Instance.LoadSounds(Content);
 
-        private void ExitEvent(object parameter)
-        {
-            Exit();
-        }
+			RelayCommand command = new RelayCommand(new Action<object>(ExitEvent));
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
-        }
+			KeyBinding keyBinding = new KeyBinding(command, KeyCode.Escape, ModifierKeys.None);
+			basicUI.InputBindings.Add(keyBinding);
+		}
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Update(GameTime gameTime)
-        {
-            basicUI.UpdateInput(gameTime);
-            basicUI.UpdateLayout(gameTime);
+		private void ExitEvent(object parameter)
+		{
+			Exit();
+		}
 
-            base.Update(gameTime);
-        }
+		/// <summary>
+		/// UnloadContent will be called once per game and is the place to unload
+		/// all content.
+		/// </summary>
+		protected override void UnloadContent()
+		{
+			// TODO: Unload any non ContentManager content here
+		}
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			debug.Update();
+			basicUI.UpdateInput(gameTime.ElapsedGameTime.TotalMilliseconds);
+			basicUI.UpdateLayout(gameTime.ElapsedGameTime.TotalMilliseconds);
 
-            basicUI.Draw(gameTime);
+			base.Update(gameTime);
+		}
 
-            base.Draw(gameTime);
-        }
-    }
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			basicUI.Draw(gameTime.ElapsedGameTime.TotalMilliseconds);
+			debug.Draw();
+			base.Draw(gameTime);
+		}
+	}
 }
