da24278064f20ab45483e9e48e7648ca0f06ce74
Empty Keys <github@emptykeys.com>
2015-11-08 18:18:23 +0100

    Hello World

---
+++ Empty_Example/Empty_MonoGame_Example/Game1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using EmptyKeys.UserInterface;
+using EmptyKeys.UserInterface.Controls;
+using EmptyKeys.UserInterface.Generated;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Empty_MonoGame_Example
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;        
+
+        private int nativeScreenWidth;
+        private int nativeScreenHeight;
+
+        private UIRoot root;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.DeviceCreated += graphics_DeviceCreated;
+            graphics.PreparingDeviceSettings += graphics_PreparingDeviceSettings;
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        void graphics_DeviceCreated(object sender, EventArgs e)
+        {
+            Engine engine = new MonoGameEngine(GraphicsDevice, nativeScreenWidth, nativeScreenHeight);
+        }
+
+        private void graphics_PreparingDeviceSettings(object sender, PreparingDeviceSettingsEventArgs e)
+        {
+            nativeScreenWidth = graphics.PreferredBackBufferWidth;
+            nativeScreenHeight = graphics.PreferredBackBufferHeight;
+
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 720;
+            graphics.PreferMultiSampling = true;
+            graphics.GraphicsProfile = GraphicsProfile.HiDef;
+            graphics.SynchronizeWithVerticalRetrace = true;
+            graphics.PreferredDepthStencilFormat = DepthFormat.Depth24Stencil8;
+            e.GraphicsDeviceInformation.PresentationParameters.MultiSampleCount = 16;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {            
+            // TODO: use this.Content to load your game content here
+
+            SpriteFont font = Content.Load<SpriteFont>("Segoe_UI_15_Bold");
+            FontManager.DefaultFont = Engine.Instance.Renderer.CreateFont(font);            
+            root = new Root();
+
+            FontManager.Instance.LoadFonts(Content);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            root.UpdateInput(gameTime.ElapsedGameTime.TotalMilliseconds);
+            root.UpdateLayout(gameTime.ElapsedGameTime.TotalMilliseconds);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            root.Draw(gameTime.ElapsedGameTime.TotalMilliseconds);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Empty_Example/GameLibrary/GeneratedUI/Root.xaml.cs
@@ -0,0 +1,65 @@
+// -----------------------------------------------------------
+//  
+//  This file was generated, please do not modify.
+//  
+// -----------------------------------------------------------
+namespace EmptyKeys.UserInterface.Generated {
+    using System;
+    using System.CodeDom.Compiler;
+    using System.Collections.ObjectModel;
+    using EmptyKeys.UserInterface;
+    using EmptyKeys.UserInterface.Charts;
+    using EmptyKeys.UserInterface.Data;
+    using EmptyKeys.UserInterface.Controls;
+    using EmptyKeys.UserInterface.Controls.Primitives;
+    using EmptyKeys.UserInterface.Input;
+    using EmptyKeys.UserInterface.Media;
+    using EmptyKeys.UserInterface.Media.Animation;
+    using EmptyKeys.UserInterface.Media.Imaging;
+    using EmptyKeys.UserInterface.Shapes;
+    using EmptyKeys.UserInterface.Renderers;
+    using EmptyKeys.UserInterface.Themes;
+    
+    
+    [GeneratedCodeAttribute("Empty Keys UI Generator", "1.10.11.0")]
+    public partial class Root : UIRoot {
+        
+        private Grid e_0;
+        
+        private TextBlock e_1;
+        
+        public Root() : 
+                base() {
+            this.Initialize();
+        }
+        
+        public Root(int width, int height) : 
+                base(width, height) {
+            this.Initialize();
+        }
+        
+        private void Initialize() {
+            Style style = RootStyle.CreateRootStyle();
+            style.TargetType = this.GetType();
+            this.Style = style;
+            this.InitializeComponent();
+        }
+        
+        private void InitializeComponent() {
+            // e_0 element
+            this.e_0 = new Grid();
+            this.Content = this.e_0;
+            this.e_0.Name = "e_0";
+            // e_1 element
+            this.e_1 = new TextBlock();
+            this.e_0.Children.Add(this.e_1);
+            this.e_1.Name = "e_1";
+            this.e_1.HorizontalAlignment = HorizontalAlignment.Center;
+            this.e_1.VerticalAlignment = VerticalAlignment.Center;
+            this.e_1.Text = "Hello World";
+            this.e_1.FontSize = 20F;
+            this.e_1.FontStyle = FontStyle.Bold;
+            FontManager.Instance.AddFont("Segoe UI", 20F, FontStyle.Bold, "Segoe_UI_15_Bold");
+        }
+    }
+}
